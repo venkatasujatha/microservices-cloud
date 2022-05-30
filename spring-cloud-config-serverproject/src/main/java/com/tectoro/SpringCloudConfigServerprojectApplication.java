7837c930aaff25e2e9b1c4487122f24016be01b2
sujatha <sujatha.valluru@tectoro.com>
2022-05-30 10:50:38 +0530

    micro services cloud

---
+++ spring-cloud-config-serverproject/src/main/java/com/tectoro/SpringCloudConfigServerprojectApplication.java
@@ -0,0 +1,13 @@
+package com.tectoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//@EnableConfi
+@SpringBootApplication
+public class SpringCloudConfigServerprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerprojectApplication.class, args);
+	}
+
+}
